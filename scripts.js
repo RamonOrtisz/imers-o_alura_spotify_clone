db99b69dabed9c0790938ba8db7e9bffb90e44bf
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-01-31 13:40:19 -0300

    try_fix

--- scripts.js
+++ scripts.js
@@ -3,32 +3,33 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
+    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
         .then((response) => response.json())
         .then((result) => displayResults(result))
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+    hiddenPlaylist(); 
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
-    result.forEach(element => {
+    result.forEach((element) => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
     });
-
     resultArtist.classList.remove('hidden');
 }
 
-document.addEventListener('input', function () {
+function hiddenPlaylist() {
+    resultPlaylist.classList.add("hidden");
+}
+
+searchInput.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
+    if (searchTerm === "") {
+        resultArtist.classList.add('hidden');
+        resultPlaylist.classList.remove('hidden');
+        return;
     }
-    
     requestApi(searchTerm);
 })
